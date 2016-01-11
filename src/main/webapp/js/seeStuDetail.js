d73ea97621532eada18046e217cd22f3c7b7eead
20131302 <Administrator@WIN-RK6HHKQVT6P>
2016-01-12 05:14:45 +0800

    字符验证

--- src/main/webapp/js/seeStuDetail.js
+++ src/main/webapp/js/seeStuDetail.js
@@ -1,44 +1,38 @@
 $(document).ready(function() {
-	
 
 });
 
-window.onload=function(){
+window.onload = function() {
 
-	
 	ShowPersonalInfo();
-	$("#upadte").click(function(){
+	$("#upadte").click(function() {
 
 		UpdateShowPersonalInfo();
-	
+
 	});
-	$("#saveIfo").click(function(){
-		
+	$("#saveIfo").click(function() {
+
 		save();
 	});
 
-	
 }
-
-
-
 
 function GetRequest() {
 	var url = location.search; // 获取url中"?"符后的字串
 	var theRequest = null;
 
 	if (url.indexOf("?") != -1) {
-		theRequest={};
+		theRequest = {};
 		var str = url.substr(1);
 		strs = str.split("&");
-		
-		for ( var i = 0; i < strs.length; i++) {
-			
+
+		for (var i = 0; i < strs.length; i++) {
+
 			theRequest.studId = strs[i].split("=")[1];
-			
+
 		}
 	}
-	
+
 	return theRequest;
 }
 
@@ -49,39 +43,33 @@ function checkValue(str) {
 	return (str);
 }
 
-
-
-
 function ShowPersonalInfo() {
 	var Request = GetRequest();
-	
-    var Userid=-1;
-   
+
+	var Userid = -1;
+
 	if (Request != null) {
 		Userid = Request.studId;
 	}
 
-    var id=Userid;
-    
-	
+	var id = Userid;
+
 	$.ajax({
 		type : "post",
 		contentType : "application/x-www-form-urlencoded;charset=UTF-8",
 		url : '/LabSys/studentController/readStudentByStudNumber.do',
 		async : false,
 		data : {
-          id:id
+			id : id
 		},
 		dataType : 'json',
 		success : function(msg) {
-		    
+
 			if (msg.result == true) {
-				if(msg.user.userType==2){
-					
-					
-					
+				if (msg.user.userType == 2) {
+
 				}
-			
+
 				$(".personalStuClass").html(msg.student.studClass);// 班级
 				$(".personalStuName").html(msg.student.userRealname);// 姓名
 				$(".personalStuNumber").html(msg.student.studNum);// 学号
@@ -89,8 +77,6 @@ function ShowPersonalInfo() {
 				$(".personalStuPhone").html(msg.student.userPhone);// 电话
 				$(".personalStuEmail").html(msg.student.userEMail);// 邮箱
 
-				
-			
 			} else {
 				alert(msg.message);
 			}
@@ -103,34 +89,31 @@ function ShowPersonalInfo() {
 
 function UpdateShowPersonalInfo() {
 	var Request = GetRequest();
-	
-    var Userid=-1;
-   
+
+	var Userid = -1;
+
 	if (Request != null) {
 		Userid = Request.studId;
 	}
 
-    var id=Userid;
-    
-	
+	var id = Userid;
+
 	$.ajax({
 		type : "post",
 		contentType : "application/x-www-form-urlencoded;charset=UTF-8",
 		url : '/LabSys/studentController/readStudentByStudNumber.do',
 		async : false,
 		data : {
-          id:id
+			id : id
 		},
 		dataType : 'json',
 		success : function(msg) {
-		    
+
 			if (msg.result == true) {
-				if(msg.user.userType==2){
-					
-					
-					
+				if (msg.user.userType == 2) {
+
 				}
-			
+
 				$(".personalStuClass").val(msg.student.studClass);// 班级
 				$(".personalStuName").val(msg.student.userRealname);// 姓名
 				$(".personalStuNumber").val(msg.student.studNum);// 学号
@@ -138,8 +121,6 @@ function UpdateShowPersonalInfo() {
 				$(".personalStuPhone").val(msg.student.userPhone);// 电话
 				$(".personalStuEmail").val(msg.student.userEMail);// 邮箱
 
-				
-			
 			} else {
 				alert(msg.message);
 			}
@@ -150,48 +131,83 @@ function UpdateShowPersonalInfo() {
 	});
 }
 
-function save(){
-var name = $("#name").val();
-var num = $("#num").val();
-var major = $("#major").val();
-var studClass = $("#studClass").val();
-var email = $("#email").val();
-var phone = $("#phone").val();
+function save() {
+	var name = $("#name").val();
+	var num = $("#num").val();
+	var major = $("#major").val();
+	var studClass = $("#studClass").val();
+	var email = $("#email").val();
+	var phone = $("#phone").val();
+	if (checkEmail() && checkPhone()) {
 
+		$.ajax({
+			type : "post",
+			contentType : "application/x-www-form-urlencoded;charset=UTF-8",
+			url : '/LabSys/studentController/updateStudent.do',
+			async : false,
+			data : {
+				name : name,
+				num : num,
+				major : major,
+				studClass : studClass,
+				email : email,
+				phone : phone
 
-      
-    	  $.ajax({
-  			type : "post",
-  			contentType : "application/x-www-form-urlencoded;charset=UTF-8",
-  			url : '/LabSys/studentController/updateStudent.do',
-  			async : false,
-  			data : {
-  				name:name,
-  				num : num,
-  				major:major,
-  				studClass:studClass,
-  				email : email,
-  				phone : phone
+			},
+			dataType : 'json',
+			success : function(msg) {
+				if (msg.result == true) {
 
-  			},
-  			dataType : 'json',
-  			success : function(msg) {
-  				if (msg.result == true) {
-  					
-  					
-  					window.location.href = 'studentDetailInfo.html';
-  					
-  				} else {
-  					alert("保存失败！");
-  				}
-  			},
-  			error : function(msg) {
-  				alert("网络超时！");
-  			}
-  		});
-     
-		
- 
+					window.location.href = 'studentDetailInfo.html';
+
+				} else {
+					alert("保存失败！");
+				}
+			},
+			error : function(msg) {
+				alert("网络超时！");
+			}
+		});
+
+	}
+
 }
 
+function checkEmail() {
 
+	var email = document.getElementById("email");
+	var value = email.value;
+	if (!value || value == null) {
+		alert("请填写邮箱！");
+		return false;
+	}
+	var pattern = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
+	flag = pattern.test(value);
+	if (flag) {
+		return true;
+	} else {
+		alert("邮箱格式不正确！");
+		return false;
+	}
+}
+
+function checkPhone() {
+	if (!document.getElementById || !document.createTextNode)
+		return false;
+	var phone = document.getElementById("phone");
+	var value = phone.value;
+	if (!value || value == null) {
+		alert("请填写电话号码！");
+		return false;
+	}
+	var pattern1 = /^(([0\+]\d{2,3}-)?(0\d{2,3})-)?(\d{7,8})(-(\d{3,}))?$/;
+	var pattern2 = /^0?(13[0-9]|15[0-9]|18[0-9]|14[57])[0-9]{8}$/;
+	flag1 = pattern1.test(value);
+	flag2 = pattern2.test(value);
+	if (flag1 == true || flag2 == true) {
+		return true;
+	} else {
+		alert("电话号码不正确！");
+		return false;
+	}
+}
